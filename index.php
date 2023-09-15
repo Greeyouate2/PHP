b03f5a071db19410001e87861dc9fa2fd52c6e74
Greeyouate2 <118512147+Greeyouate2@users.noreply.github.com>
2023-09-15 13:53:43 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+   <title>PHP TEST</title> 
+  </head>
+  <body>
+    <header>
+      <h1>
+        HELLO
+      </h1>
+    <h9>
+      PHP Testing
+    </h9>
+      <nav>
+       <a href="https://greeyousoft2.github.io/">
+         Home(GreeyouSoft)
+       </a>
+      </nav>
+    </header>
+  </body>
+</html>
